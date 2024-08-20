125f4ac43f5cd70fbdbc070faf3b9d0b2dddaf00
FrankXav <framirezl1701@alumno.ipn.mx>
2024-08-20 14:00:04 -0600

    Nueva url POST

--- Web/Proyecto/webAire/webAire/settings.py
+++ Web/Proyecto/webAire/webAire/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'apicalidadaire',
     'core'
 ]
@@ -141,5 +142,3 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-print(BASE_DIR,"hola esta la base")
--- Web/Proyecto/webAire/webAire/urls.py
+++ Web/Proyecto/webAire/webAire/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from core import views  
+from django.urls import include, path
+
+from apicalidadaire import views as vapi
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('pronostico/', views.pronostico, name="pronostico"),
     path('conocenos/', views.conocenos, name="conocenos"),
     path('contacto/', views.contacto, name="contacto"),
     path('admin/', admin.site.urls),
+    path("pronostico/api/", include("apicalidadaire.urls"))
+    #path('prediccion/', vapi.prediccion, name="prediccion")
 ]
