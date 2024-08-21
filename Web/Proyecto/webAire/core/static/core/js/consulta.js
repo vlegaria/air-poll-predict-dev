1c94207acb287c479bab47b3720d33d6555b1b76
FrankXav <framirezl1701@alumno.ipn.mx>
2024-08-20 19:30:33 -0600

    Orden a consulta

--- Web/Proyecto/webAire/core/static/core/js/consulta.js
+++ Web/Proyecto/webAire/core/static/core/js/consulta.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
+
+
     $('#pronosticar').click(function(e){
         var contaminante = $('#contaminante').val();
         var prediccion1 = $('#1hora').is(':checked');
         var prediccion24 = $('#24horas').is(':checked');
-
-        console.log("contaminante: " + contaminante)
-        console.log("prediccion1: " + prediccion1)
-        console.log("prediccion24: " + prediccion24)
 
         if(!prediccion1 && !prediccion24){
             alert("Seleccione una opción para la predicción!")
@@ -16,8 +14,8 @@ $(document).ready(function(){
                 type: 'POST',
                 data: {contaminante,prediccion1,prediccion24},
                 dataType: 'json',
-                success: function(resultado){	
-                    alert(resultado)
+                success: function(response){	
+                    alert(response)
                 }
             });
         }
