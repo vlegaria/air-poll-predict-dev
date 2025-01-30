360368f89fc8397e63e2628ad2ea417eed2a9be2
vlegaria <vlegaria.s@gmail.com>
2025-01-30 15:59:57 +0000

    ruta modelos

--- ML/Notebooks/upload_models2mlflow.py
+++ ML/Notebooks/upload_models2mlflow.py
@@ -9,8 +9,10 @@ import mlflow.pyfunc
 from config.config import MLFLOW_PROJECT, MLFLOW_PWD, MLFLOW_USER, historical_data_1hrfuture, RUTA_MODELOS
 from utils.utils import *
 from sklearn.model_selection import train_test_split
+import os
+mlflow.set_tracking_uri(uri="http://0.0.0.0:5000")
+#mlflow.set_tracking_uri(uri="http://geopiig.com:5000")
 
-mlflow.set_tracking_uri(uri="http://127.0.0.1:5000")
 client = MlflowClient()
 stations = ['MER', 'UIZ']
 times_future = [1,24]
@@ -23,9 +25,18 @@ for time_future in times_future:
         # Se carga el archivo de los modelos y del scaler por cada estacion, el target, y el tiempo a predecir
         model_dir = RUTA_MODELOS+'root/web/air-poll-predict-dev/ML/Modelos/best_model_XGBoost_'+str(time_steps)+'timesteps_O3_'+str(time_future)+'timefuture_'+station+'.pkl'
         model_dir = RUTA_MODELOS+'air-poll-predict-dev/ML/Modelos/best_model_XGBoost_'+str(time_steps)+'timesteps_O3_'+str(time_future)+'timefuture_'+station+'.pkl'
+        model_dir = 'best_model_XGBoost_'+str(time_steps)+'timesteps_O3_'+str(time_future)+'timefuture_'+station+'.pkl'
+        # Obtener la ruta absoluta del script actual
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        # Subir un nivel y acceder a la carpeta 'datos'
+        ruta_archivo = os.path.join(script_dir, "..", "Modelos", model_dir)
+        # Convertir la ruta en absoluta (para evitar problemas)
+        model_dir = os.path.abspath(ruta_archivo)
+        print(model_dir)
+
         loaded_model = joblib.load(model_dir)
-        dir_scaler = RUTA_MODELOS+'root/web/air-poll-predict-dev/ML/Scalers/'+station+'_scaler.pkl'
-        #dir_scaler = RUTA_MODELOS+'air-poll-predict-dev/ML/Scalers/'+station+'_scaler.pkl'
+        #dir_scaler = RUTA_MODELOS+'root/web/air-poll-predict-dev/ML/Scalers/'+station+'_scaler.pkl'
+        dir_scaler = RUTA_MODELOS+'air-poll-predict-dev/ML/Scalers/'+station+'_scaler.pkl'
         with open(dir_scaler, 'rb') as file:
             loaded_scaler = pickle.load(file)
 
@@ -38,10 +49,13 @@ for time_future in times_future:
         X_seq, y_seq = create_sequences2(X, y, time_steps, time_future)
         X_train, X_test, y_train, y_test = train_test_split(X_seq, y_seq, test_size=0.2, random_state=42)
         predicciones_normalizadas = loaded_model.predict(X_test)
-        predicciones_normalizadas = predicciones_normalizadas.reshape(-1, 1)
-        predicciones = loaded_scaler.inverse_transform(predicciones_normalizadas)
-        y_test_normalizadas = y_test.reshape(-1, 1)
-        y_test = loaded_scaler.inverse_transform(y_test_normalizadas)
+        min_val = loaded_scaler.data_min_[4]  # Valor mínimo del O3
+        max_val = loaded_scaler.data_max_[4]  # Valor máximo del O3
+        # Aplicar la transformación inversa 
+        predicciones = predicciones_normalizadas * (max_val - min_val) + min_val
+        #y_test_normalizadas = y_test.reshape(-1, 1)
+        #y_test = loaded_scaler.inverse_transform(y_test_normalizadas)
+        y_test = y_test * (max_val - min_val) + min_val
         metrics_results = metrics(X_test, y_test, predicciones, printData=False)
 
         MLFLOW_experiment = f"{target} {time_future}hr forecast {station}"
